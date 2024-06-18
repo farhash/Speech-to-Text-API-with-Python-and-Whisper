711acb4e13fa378fc55fb4cba212305be0459014
Arshiya Farhat <arshiyafarhat81@gmail.com>
2024-06-18 11:38:45 +0330

    two model proccessors to switch and use at will

---
+++ local_model_proccess.py
@@ -0,0 +1,48 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from timeit import default_timer as timer
+import joblib
+
+start_time = timer()
+
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+model_id = "/media/farhat/4cde39e4-2700-4061-a127-7c28b1cd563c/arshiya/models/whisper-medium-fa"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+
+end_time2 = timer()
+print(f"after model to device : {end_time2-start_time:.3f} seconds")
+
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+end_time3 = timer()
+print(f"after processor : {end_time3-start_time:.3f} seconds")
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=15,
+    batch_size=16,
+    return_timestamps=True,
+    torch_dtype=torch_dtype,
+    device=device,
+)  
+end_time4 = timer()
+print(f"after pipe : {end_time4-start_time:.3f} seconds") 
+
+filename = "Completed_model.joblib"
+joblib.dump(pipe, filename)
+
+
+end_time = timer()
+print(f"[INFO] Total training time: {end_time-start_time:.3f} seconds")
