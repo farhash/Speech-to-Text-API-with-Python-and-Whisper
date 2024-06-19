6fc5edde6c102be43d0d2aeee8ca8b46174d9956
Arshiya Farhat <arshiyafarhat81@gmail.com>
2024-06-19 11:27:57 +0330

    edit local model proccessor file to make it more readable

--- local_model_proccess.py
+++ local_model_proccess.py
@@ -8,7 +8,7 @@ start_time = timer()
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-model_id = "/media/farhat/4cde39e4-2700-4061-a127-7c28b1cd563c/arshiya/models/whisper-medium-fa"
+model_id = "path_of_your_local_whisper_model"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
