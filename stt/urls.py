d0b3e44f281390c533e04bc35c3d02c82adbbb97
Arshiya Farhat <arshiyafarhat81@gmail.com>
2024-07-22 11:30:52 +0330

    fix urls

--- stt/urls.py
+++ stt/urls.py
@@ -4,9 +4,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
-  path('stt/url', views.SttGeneral.as_view()),
-  path('stt/upload/', views.upload_file),
-  path('stt/result/<stt_result_key>', views.SttDetail.as_view()),
+  path('url/', views.SttGeneral.as_view()),
+  path('upload/', views.upload_file),
+  path('result/<stt_result_key>/', views.SttDetail.as_view()),
   path('schema/', SpectacularAPIView.as_view(), name='schema'),
   path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
